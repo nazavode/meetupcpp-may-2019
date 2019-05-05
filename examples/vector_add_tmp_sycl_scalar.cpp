068a025cfd88c758c2f460b481d388836512bda4
Federico Ficarelli <federico.ficarelli@gmail.com>
2019-05-05 11:36:01 +0200

    Fix scalar add template integral + real

--- examples/vector_add_tmp_sycl_scalar.cpp
+++ examples/vector_add_tmp_sycl_scalar.cpp
@@ -10,6 +10,8 @@
 
 using namespace cl;
 
+namespace {
+
 template <typename T>
 struct AddKernel {};
 
@@ -19,7 +21,8 @@ std::vector<T> add(const std::vector<T>& a, const std::vector<T>& b) {
     std::vector<T> result(std::min(std::size(a), std::size(b)));
     if (result.size() == 0) return {};
 
-    {  // Open a new scope to bind the lifetime of the command queue.
+    // Open a new scope to bind the lifetime of the command queue.
+    {
         // Queue's destructor will wait for all pending operations to complete.
         sycl::queue queue;
 
@@ -67,31 +70,59 @@ std::vector<T> make_dataset(std::size_t size) {
     return dataset;
 }
 
+}  // namespace
+
 int main(int argc, char** argv) {
     if (argc < 2) {
         std::cout << "Usage: " << argv[0] << " <items count>" << std::endl;
         return 1;
     }
     const auto count = std::stoll(argv[1]);
-    using element_type = int;
-    const auto a = make_dataset<element_type>(count);
-    const auto b = make_dataset<element_type>(count);
-    const auto result = add(a, b);
-    // Check correctness
-    const auto equals = [](element_type lhs, element_type rhs) {
-        if constexpr (std::is_floating_point_v<element_type>) {
+    // Equality checker predicate
+    const auto equals = [](auto a, auto b) {
+        using value_type = decltype(a);
+        if constexpr (std::is_floating_point_v<value_type>) {
             // Quick and dirty, don't try this at home.
             // If you really want to compare floating points in the real world, have a
             // look here: https://www.itu.dk/~sestoft/bachelor/IEEE754_article.pdf
-            return std::abs(lhs - rhs) < std::numeric_limits<element_type>::epsilon();
+            return std::abs(a - b) < std::numeric_limits<value_type>::epsilon();
         } else {
-            return lhs == rhs;
+            return a == b;
         }
     };
-    for (decltype(std::size(result)) i = 0; i < std::size(result); ++i) {
-        if (!equals(result[i], a[i] + b[i])) {
-            std::cerr << "diff at index " << i << ": " << result[i]
-                      << " != " << a[i] + b[i] << std::endl;
+    // Integral
+    {
+        const auto a = make_dataset<int>(count);
+        const auto b = make_dataset<int>(count);
+        const auto result = add(a, b);
+        // Correctness check
+        auto expected = std::vector<int>(count);
+        std::transform(std::begin(a), std::end(a), std::begin(b), std::begin(expected),
+                       std::plus<>{});
+        auto [result_it, expected_it] = std::mismatch(
+            std::begin(result), std::end(result), std::begin(expected), equals);
+        if (result_it != std::end(result)) {
+            const auto diff_idx = std::distance(std::begin(result), result_it);
+            std::cerr << "Integral: diff at index " << diff_idx << ": " << *result_it
+                      << " != " << *expected_it << std::endl;
+            return 1;
+        }
+    }
+    // Real
+    {
+        const auto a = make_dataset<float>(count);
+        const auto b = make_dataset<float>(count);
+        const auto result = add(a, b);
+        // Correctness check
+        auto expected = std::vector<float>(count);
+        std::transform(std::begin(a), std::end(a), std::begin(b), std::begin(expected),
+                       std::plus<>{});
+        auto [result_it, expected_it] = std::mismatch(
+            std::begin(result), std::end(result), std::begin(expected), equals);
+        if (result_it != std::end(result)) {
+            const auto diff_idx = std::distance(std::begin(result), result_it);
+            std::cerr << "Real: diff at index " << diff_idx << ": " << *result_it
+                      << " != " << *expected_it << std::endl;
             return 1;
         }
     }
